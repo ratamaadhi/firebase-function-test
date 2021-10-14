ef97a4ba427920ff6739955402512504d20db94e
Ratama Adhi <ratama.adhi@gmail.com>
2021-10-14 16:26:32 +0700

    progress register dan login

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,26 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
-    getAllTodos
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
 } = require('./APIs/todos')
 
+const {
+    loginUser,
+    signUpUser
+} = require('./APIs/users')
+
+//Todos
+app.get('/', getAllTodos);
 app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
+
+//Users
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
+
 exports.api = functions.https.onRequest(app);
